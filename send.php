38dfe74da9d368d7d35a328031c38f52e643654f
ksoz <kylesoz@protonmail.com>
2021-10-03 02:18:17 +0000

    trying to re-align things

--- send.php
+++ send.php
@@ -3,7 +3,7 @@ require_once __DIR__ . '/vendor/autoload.php';
 use PhpAmqpLib\Connection\AMQPStreamConnection;
 use PhpAmqpLib\Message\AMQPMessage;
 //This is an example from offical RabbitMQ documentation & tutorials
-$connection = new AMQPStreamConnection('localhost', 5672, 'guest', 'guest');
+$connection = new AMQPStreamConnection('18.223.223.39', 5672, 'admin', 'kwsadmin');
 $channel = $connection->channel();
 $channel->queue_Declare('hello', false, false, false, false);
 
