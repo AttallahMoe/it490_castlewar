ff2be7c278a09000fdd625dcef679489f1f49841
ksoz <kylesoz@protonmail.com>
2021-10-04 03:09:01 +0000

    changed queues to test

--- worker.php
+++ worker.php
@@ -5,7 +5,7 @@ use PhpAmqpLib\Connection\AMQPStreamConnection;
 $connection = new AMQPStreamConnection('localhost', 5672, 'guest', 'guest');
 $channel = $connection->channel();
 
-$channel->queue_declare('hello', false, false, false ,false);
+$channel->queue_declare('test', false, false, false ,false);
 echo "[*] Waiting for messages. To exit press CTRL+C\n";
 
 $callback = function ($msg) {
@@ -14,7 +14,7 @@ $callback = function ($msg) {
     echo "[x] Done\n";
 };
 
-$channel->basic_consume('hello', '', false, true, false, false, $callback);
+$channel->basic_consume('test', '', false, true, false, false, $callback);
 
 while ($channel->is_open()) {
     $channel->wait();
