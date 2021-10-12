c58589978030e43c643a2f61d3a7b815495e0cec
ksoz <kylesoz@protonmail.com>
2021-10-12 19:38:41 +0000

    new stuff

--- new_task.php
+++ new_task.php
@@ -5,13 +5,16 @@ use PhpAmqpLib\Message\AMQPMessage;
 //This is an example from offical RabbitMQ documentation & tutorials
 $connection = new AMQPStreamConnection('18.216.70.84', 5672, 'admin', 'kwsadmin');
 $channel = $connection->channel();
-$channel->queue_Declare('test', false, false, false, false);
+$channel->queue_Declare('task_queue', false, true, false, false);
 
 $data = implode(' ', array_slice($argv, 1));
 if (empty($data)) {
 	$data = "Hello World!";
 }
-$msg = new AMQPMessage($data);
+$msg = new AMQPMessage(
+    $data,
+    array('delivery_mode' => AMQPMessage::DELIVERY_MODE_PERSISTENT)
+);
 $channel->basic_publish($msg, '', 'test');
 
 echo '[x] Sent ' , $data, "\n";
